0632578dcd80ad998da39f46095302d627098ab2
Kelvince01 <themikrochip1@gmail.com>
2022-10-11 10:27:09 +0300

    added user class

---
+++ classes/user.py
@@ -0,0 +1,11 @@
+class User:
+    def __init__(self):
+        self.id = 0
+	self.username = ''
+	self.password = ''
+
+    def register(email, password):
+	pass
+
+    def login(email, password):
+	pass
