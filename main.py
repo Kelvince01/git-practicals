345f0c7e786c6db87eb683e5a61218c82b99005f
Kelvince01 <themikrochip1@gmail.com>
2022-10-11 10:20:01 +0300

    added main features

---
+++ main.py
@@ -0,0 +1,11 @@
+from time import time
+import json
+import requests
+from urllib.parse import urlparse
+from uuid import uuid4
+
+def main():
+    print('Executing main')
+
+if __name__ == '__main__':
+    main()
